4fd2934b893b70e00998212b082edf579ab5b42e
Ilona <150517376+LunyaLp@users.noreply.github.com>
2024-03-10 19:11:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,22 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int leftBorder = 0;
-  int rightBorder = size - 1;
-  int counter = 0;
-  while(leftBorder <= rightBorder){
+  uint16_t leftBorder = 0;
+  uint16_t rightBorder = size - 1;
+  uint16_t counter = 0;
+  while (leftBorder <= rightBorder) {
     int part = (leftBorder + rightBorder)/2;
-    if(arr[part] == value){
+    if (arr[part] == value) {
       counter++;
     }
-    if(arr[part] > value){
+    if (arr[part] > value) {
       rightBorder = part-1;
     }else{
-      if(value > arr[part]){
+      if (value > arr[part]) {
         leftBorder = part+1;
       }
     }
-    if((leftBorder > rightBorder) && (counter == 0)){
+    if ((leftBorder > rightBorder) && (counter == 0)) {
       return 0;
     }
   }
