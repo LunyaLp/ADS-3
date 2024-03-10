a676b842343f758eb542a268e388dc5ca035a4c4
Ilona <150517376+LunyaLp@users.noreply.github.com>
2024-03-10 18:54:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int leftBorder = 0;
+  int rightBorder = size - 1;
+  int counter = 0;
+  while(leftBorder <= rightBorder){
+    int part = (leftBorder + rightBorder)/2;
+    if(arr[part] == value){
+      counter++;
+    }
+    if(arr[part] > value){
+      rightBorder = part-1;
+    }else{
+      if(value > arr[part]){
+        leftBorder = part+1;
+      }
+    }
+    if((leftBorder > rightBorder) && (counter == 0)){
+      return 0;
+    }
+  }
+  return counter;
 }
